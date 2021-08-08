4240677ad657c4cfe0ece0a050d1db7473682a08
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-08 23:33:14 +0100

    Fix: fix ping and view limit button clicks not registering

--- src/ui/Truck Viewer.gd
+++
@@ -1,9 +0,0 @@
-extends ViewportContainer
-
-onready var viewport: Viewport = $TruckViewport
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouse:
-		event.position -= rect_global_position
-	viewport.unhandled_input(event)
