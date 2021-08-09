3346a968f4f5b0e828c838cbdd31bfe790b51155
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-09 17:58:18 +0100

    Feat: add warnings parser

---
+++ src/misc_scripts/caution_limits.gd
@@ -0,0 +1,12 @@
+class_name CautionLimits
+extends Object
+
+const MIN_TYRE_PRESSURE := 100  # psi
+const MAX_TYRE_PRESSURE := 120  # psi
+
+const MIN_BRAKE_PADS := 20  # %
+
+const MAX_WHEEL_BEARING_TEMP := 50  # deg C
+
+const MAX_WEIGHT := 600  # kg - On any 1 sensor
+const MAX_WEIGHT_DIFFERENTIAL := 70  # kg
---
+++ src/misc_scripts/critical_limits.gd
@@ -0,0 +1,15 @@
+class_name CriticalLimits
+extends Object
+
+const MIN_TYRE_PRESSURE := 95  # psi
+const MAX_TYRE_PRESSURE := 125  # psi
+
+const MIN_BRAKE_PADS := 15  # %
+
+const MAX_WHEEL_BEARING_TEMP := 55  # deg C
+
+const MAX_WEIGHT := 650  # kg - On any 1 sensor
+const MAX_WEIGHT_DIFFERENTIAL := 90  # kg
+
+const MIN_FREEZER_TEMP := -4  # deg C - While in use
+const MAX_FRIDGE_TEMP := 6  # deg C - While in use
