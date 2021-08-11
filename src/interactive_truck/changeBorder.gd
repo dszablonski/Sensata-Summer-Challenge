f711215fb3a6fef160893c51b782e8b5f077f15c
Dominik <dszablonski298@c2ken.net>
2021-08-11 13:02:35 +0100

    You can now select all parts of the truck

--- src/interactive_truck/changeBorder.gd
+++ src/interactive_truck/changeBorder.gd
@@ -11,6 +11,10 @@ onready var meshInstance
 # but am having a little bit of trouble 
 #var last_node_clicked
 
+func clear_borders(node, colour):
+	for i in node.get_parent().get_children():
+		change_border(i, colour)
+
 # Function to change the border
 func change_border(get_mesh, get_colour):
 	print("Border texture changed")
@@ -18,15 +22,83 @@ func change_border(get_mesh, get_colour):
 	var material = get_mesh.get_surface_material(0)
 	# Sets the albedo of the shader to the colour passed into the function
 	material.set_shader_param("albedo", get_colour)
-		
+
+func main(meshInstance):
+	clear_borders(meshInstance, clear)
+	change_border(meshInstance, selected)
 
 # For now, this only applies to Truck Wheel A and the freezer
 func _on_StaticBody_truck_wheel_a():
-	meshInstance = self.get_node(".")
-	change_border(meshInstance, selected)
+	main(self.get_node("."))
 
 
 
 func _on_StaticBody_freezer():
-	meshInstance = self.get_node(".")
-	change_border(meshInstance, selected)
+	main(self.get_node("."))
+
+
+func _on_StaticBody_brake_wear_sensor_ab():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_clear():
+	clear_borders(self.get_node("."), clear)
+
+func _on_StaticBody_truck_wheel_b():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_trailer_wheel_c():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_truck_wheel_d():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_truck_wheel_e():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_truck_wheel_f():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_brake_wear_sensor_eb():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_brake_wear_sensor_da():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_brake_wear_sensor_fc():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_trailer_wheel_a():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_trailer_wheel_b():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_trailer_wheel_d():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_trailer_wheel_e():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_trailer_wheel_f():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_fridge():
+	main(self.get_node("."))
+
+
+func _on_StaticBody_truck_wheel_c():
+	main(self.get_node("."))
