ff183bfcace23ec0f68a6cf44bf19da9c850f426
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-11 13:32:37 +0100

    Feat: move zip contents into src/

--- src/autoload/database_fetch.gd
+++ src/autoload/database_fetch.gd
@@ -4,6 +4,7 @@ var db
 var db_path="res://Database/database.db"
 var tablename="Database"
 onready var timer=get_node("Timer")
+var ID=1
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -13,15 +14,16 @@ onready var timer=get_node("Timer")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	db=SQLite.new()
-	db.open_db()
+	db.path=db_path
+	db.open_db(db)
 	timer.set_wait_time(1)
 	timer.start()
 
 func readfromdb():
-	db.query("select * from" + tablename + "where ID")
-	var telemetry_data=db.query_result()
-	telemetry_data=Dictionary(telemetry_data)
+	db.query("select * from " + tablename + " where ID=" + str(ID))
+	var telemetry_data=db.query_result
 	print(telemetry_data)
+	ID=ID+1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
