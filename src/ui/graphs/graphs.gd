518ca523b07dfe471e58cd0c9ce572cdb13f2a01
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-16 02:28:15 +0100

    Feat: add chart selection

--- src/ui/graphs/graphs.gd
+++ src/ui/graphs/graphs.gd
@@ -2,10 +2,22 @@ extends PanelContainer
 
 const PLACEHOLDER_DAY := 5
 
-onready var line_chart: LineChart = $LineChart
+var _selected_chart: Chart
+var _selected_chart_old_x_decim: int
+
+onready var graphs_grid: GridContainer = $GraphsGrid
+onready var graphs_grid_columns_default = graphs_grid.columns
 
 
 func _ready() -> void:
+	for chart in graphs_grid.get_children():
+		chart = chart as LineChart
+		chart.connect("clicked", self, "_on_LineChart_clicked", [chart])
+		chart.origin_at_zero = true
+	_plot_data()
+
+
+func _plot_data(specific_chart: Chart = null) -> void:
 	var formatted_data := [
 		[0],  # I don't have a clue what this 0 is for but changing it seems to do nothing
 		[""],
@@ -23,5 +35,29 @@ func _ready() -> void:
 		var average_weight := int(total_weight_acting_on_sensors / num_weight_sensors)
 		formatted_data[0].append(i)
 		formatted_data[1].append(average_weight)
-	line_chart.origin_at_zero = true
-	line_chart.plot_from_array(formatted_data)
+	if specific_chart:
+		specific_chart.plot_from_array(formatted_data)
+	else:
+		for chart in graphs_grid.get_children():
+			chart = chart as LineChart
+			chart.plot_from_array(formatted_data)
+
+
+func _on_LineChart_clicked(chart: LineChart) -> void:
+	if chart == _selected_chart:
+		graphs_grid.columns = graphs_grid_columns_default
+		for graph in graphs_grid.get_children():
+			graph.visible = true
+		_selected_chart.x_decim = _selected_chart_old_x_decim
+		_selected_chart.show_points = false
+		_plot_data(_selected_chart)
+		_selected_chart = null
+	else:
+		_selected_chart = chart
+		graphs_grid.columns = 1
+		for graph in graphs_grid.get_children():
+			graph.visible = graph == _selected_chart
+		_selected_chart_old_x_decim = _selected_chart.x_decim
+		_selected_chart.x_decim = 1
+		_selected_chart.show_points = true
+		_plot_data(_selected_chart)
