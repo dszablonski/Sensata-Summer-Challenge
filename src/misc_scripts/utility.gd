347391527c0c0bd512340e4dc3e5b35a0440ddbb
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-19 17:46:41 +0100

    Docs: add comments to the utility script

--- src/misc_scripts/utility.gd
+++ src/misc_scripts/utility.gd
@@ -2,6 +2,10 @@ class_name Util
 extends Object
 
 static func to_time_string(num_hours: float) -> String:
+	# Converts the number of hours into a string.
+	# For example, hour 0 means midnight so it would be "00:00".
+	# Hour 23 would be "23:00".
+	# It also works with fractional hours so 3.5 would be "03:30".
 	var whole_hours := int(num_hours)
 	var remaining_hours := num_hours - whole_hours
 	var remaining_mins := remaining_hours * 60
