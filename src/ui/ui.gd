0c2866fc860da90cd2b42092721b26242ebfc3d5
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-20 01:49:59 +0100

    Feat: unpack Andy's timeline

--- src/ui/ui.gd
+++ src/ui/ui.gd
@@ -1,16 +1,16 @@
 extends MarginContainer
 
-export var time := 0.0
-
-onready var date_time_label: Label = $VSplitContainer/HSplitContainer/LeftSide/DateTime
-onready var clock: Clock = $"VSplitContainer/HSplitContainer/RightSide/TabContainer/Time Selection"
-
-
-func _ready() -> void:
-	date_time_label.setup(time)
-	clock.force_press_time_button(time)
-
-
-func _on_Time_Selection_time_changed(new_time) -> void:
-	time = new_time
-	date_time_label.setup(time)
+#export var time := 0.0
+#
+#onready var date_time_label: Label = $VSplitContainer/HSplitContainer/LeftSide/DateTime
+#onready var clock: Clock = $"VSplitContainer/HSplitContainer/RightSide/TabContainer/Time Selection"
+#
+#
+#func _ready() -> void:
+#	date_time_label.setup(time)
+#	clock.force_press_time_button(time)
+#
+#
+#func _on_Time_Selection_time_changed(new_time) -> void:
+#	time = new_time
+#	date_time_label.setup(time)
