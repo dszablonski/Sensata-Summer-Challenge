b349022972ddf2f0b3bba36e8a3e68f1017b5d56
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-20 20:32:45 +0100

    Feat: make clock take data from the database

--- src/ui/clock/clock.gd
+++ src/ui/clock/clock.gd
@@ -4,6 +4,8 @@ extends Control
 
 signal time_changed(new_time)
 
+const PLACEHOLDER_DAY := 3
+
 const TIME_BUTTON_SCENE := preload("res://ui/clock/time_button.tscn")
 
 const IS_ANTIALIASED := true
@@ -19,51 +21,22 @@ const TIME_NUM_SEGMENTS := 24
 
 const SAFE_COLOR := Color("63c74d")
 const WARNING_COLOR := Color("feae34")
-const DANGER_COLOR := Color("cc2530")
+const CRITICAL_COLOR := Color("cc2530")
 
 const SAFETY_VALUE_TO_COLOR := {
-	1.0: SAFE_COLOR,
-	0.5: WARNING_COLOR,
-	0.0: DANGER_COLOR,
+	0: SAFE_COLOR,
+	1: WARNING_COLOR,
+	2: CRITICAL_COLOR,
 }
 
-# Placeholder values while the database is still being worked on
-# 1 is safe
-# 0.5 is a warning
-# 0 is dangerous
-export (Array, int) var hour_safety_values := [
-	1.0,  # 00:00
-	1.0,  # 01:00
-	1.0,  # 02:00
-	1.0,  # 03:00
-	1.0,  # 04:00
-	1.0,  # 05:00
-	1.0,  # 06:00
-	1.0,  # 07:00
-	1.0,  # 08:00
-	1.0,  # 09:00
-	1.0,  # 10:00
-	1.0,  # 11:00
-	1.0,  # 12:00
-	1.0,  # 13:00
-	1.0,  # 14:00
-	1.0,  # 15:00
-	1.0,  # 16:00
-	1.0,  # 17:00
-	1.0,  # 18:00
-	1.0,  # 19:00
-	1.0,  # 20:00
-	1.0,  # 21:00
-	1.0,  # 22:00
-	1.0,  # 23:00
-] setget _set_hour_safety_values
-
+var _hour_safety_values: Array
 var _center: Vector2
 var _clock_radius: float
 var _time_to_buttons := {}
 
 
 func _ready() -> void:
+	_hour_safety_values = _get_hour_safety_values()
 	_add_time_buttons()
 	_on_rect_changed()
 
@@ -78,6 +51,94 @@ func force_press_time_button(time: float) -> void:
 	var button: Button = _time_to_buttons[time]
 	button.pressed = true
 	button.disabled = true
+
+
+func _get_hour_safety_values() -> Array:
+	var hour_safety_values := []
+	# Iterating through every hour (0 to 23).
+	for i in 24:
+		var safety_value := 0
+		# Grabbing the data for that hour.
+		var hourly_data := DatabaseFetch.read_db_time(PLACEHOLDER_DAY, i)
+		hourly_data.erase("ID")
+		hourly_data.erase("DateTime")
+		hourly_data.erase("Hour")
+		var trailer_weights := Util.get_trailer_weights(hourly_data)
+		var has_cargo := Util.has_cargo(trailer_weights)
+		for sensor in hourly_data:
+			var value: int = hourly_data[sensor]
+			var identifier: String = sensor[-1]
+			var temp_safety_value: int
+			if "TyrePressure" in sensor:
+				temp_safety_value = _get_safety_value(
+					value,
+					CriticalLimits.MIN_TYRE_PRESSURE,
+					CautionLimits.MIN_TYRE_PRESSURE,
+					CriticalLimits.MAX_TYRE_PRESSURE,
+					CautionLimits.MAX_TYRE_PRESSURE
+				)
+			elif "BrakePads" in sensor:
+				temp_safety_value = _get_safety_value(
+					value,
+					CriticalLimits.MIN_BRAKE_PADS,
+					CautionLimits.MIN_BRAKE_PADS
+				)
+			elif "TruckWheelBearingTemp" in sensor:
+				temp_safety_value = _get_safety_value(
+					value,
+					null,
+					null,
+					CriticalLimits.MAX_WHEEL_BEARING_TEMP,
+					CautionLimits.MAX_WHEEL_BEARING_TEMP
+				)
+			elif "TrailerTemperature" in sensor:
+				if not has_cargo:
+					continue
+				if identifier in ["A", "B", "C"]:
+					temp_safety_value = _get_safety_value(
+						value,
+						CriticalLimits.MIN_FREEZER_TEMP
+					)
+				elif identifier in ["D", "E", "F"]:
+					temp_safety_value = _get_safety_value(
+						value,
+						null,
+						null,
+						CriticalLimits.MAX_FRIDGE_TEMP
+					)
+			elif "TrailerWeight" in sensor:
+				temp_safety_value = _get_safety_value(
+					value,
+					null,
+					null,
+					CriticalLimits.MAX_WEIGHT,
+					CautionLimits.MAX_WEIGHT
+				)
+			if temp_safety_value > safety_value:
+				safety_value = temp_safety_value
+				break
+		hour_safety_values.append(safety_value)
+	return hour_safety_values
+
+
+func _get_safety_value(
+	value: int,
+	min_value_critical = null,
+	min_value_caution = null,
+	max_value_critical = null,
+	max_value_caution = null
+) -> int:
+	if (
+		(min_value_critical and value < min_value_critical) or
+		(max_value_critical and value > max_value_critical)
+	):
+		return 2
+	elif (
+		(min_value_caution and value < min_value_caution) or
+		(max_value_caution and value > max_value_caution)
+	):
+		return 1
+	return 0
 
 
 func _add_time_buttons() -> void:
@@ -129,7 +190,7 @@ func _draw_hour_safety_sectors() -> void:
 	for i in range(24):
 		var start_angle := hour_angle * i - TAU / 4
 		var end_angle := hour_angle * (i + 1) - TAU / 4
-		var safety_value: float = hour_safety_values[i]
+		var safety_value: int = _hour_safety_values[i]
 		var color: Color = SAFETY_VALUE_TO_COLOR[safety_value]
 		_draw_sector(_center, _clock_radius, start_angle, end_angle, color, IS_ANTIALIASED)
 
@@ -137,8 +198,8 @@ func _draw_hour_safety_sectors() -> void:
 func _draw_hour_safety_segments() -> void:
 	var hour_angle := TAU / 24
 	for i in range(24):
-		var prev_safety_value: float = hour_safety_values[(i - 1) % 24]
-		var safety_value: float = hour_safety_values[i]
+		var prev_safety_value: float = _hour_safety_values[(i - 1) % 24]
+		var safety_value: float = _hour_safety_values[i]
 		if safety_value != prev_safety_value:
 			var angle := hour_angle * i
 			var dir := Vector2.UP.rotated(angle)
@@ -168,8 +229,3 @@ func _draw_sector(
 
 func _on_Clock_item_rect_changed() -> void:
 	_on_rect_changed()
-
-
-func _set_hour_safety_values(value: Array) -> void:
-	hour_safety_values = value
-	update()
