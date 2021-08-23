4b9da5985defe7c2152d5d8d3272accfc7159205
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-23 16:15:13 +0100

    Feat: unpack Timeline v1.3 into src

--- src/ui/Button6.gd
+++ src/ui/Button6.gd
@@ -6,30 +6,132 @@ var Icon_Yellow=preload("res://calender_yellow.png")
 var Icon_Orange=preload("res://calender_orange.png")
 var Icon_Red=preload("res://calender_red.png")
 var Icon_Really_Red=preload("res://calender_really_red.png")
+func updateColourLeft():
+	var difference=-2#Makes it go back 6 days
+	var day=GlobalDate.StartDay
+	var month=GlobalDate.StartMonth
+	var year=GlobalDate.StartYear#
+	day = day+difference#Gets the date this button will input
+	if day<1:#checks if the day is in the day is in the same month
+		month=month-1#if it's not it goes back a month 
+		if month<1:#this checks if the month is in the same year
+			year=year-1#this goes back a year 
+			month=12#this sends the date to december (the last month)
+			day=day+31#this restores the date 
+		else:#if the month is in the year
+			GetDaysInMonth(month)#it gets the amount of days are in the month
+			day=day+GlobalDate.DaysInMonth#and adds them to restore the date
+	if year>=GlobalDate.LowerYear:#checks if the year is in range of the minium date
+		if month>=GlobalDate.LowerMonth:#checks if the month is in range of the minium date
+			if day>=GlobalDate.LowerDay:#checks if the month is in the range of the minium date
+				randomize()
+				GlobalDate.CautionLevel6=int(rand_range(1,5))
+			else:
+				GlobalDate.CautionLevel6=0
+		else:
+			GlobalDate.CautionLevel6=0
+	else:
+		GlobalDate.CautionLevel6=0
+	Change_colour()
+
+func UpdateColourRight():
+	var difference=-2#Makes it go back 2 days
+	var day=GlobalDate.StartDay
+	var month=GlobalDate.StartMonth
+	var year=GlobalDate.StartYear#
+	day = day+difference#Gets the date this button will input
+	GetDaysInMonth(month)
+	if day>GlobalDate.DaysInMonth:
+		month=month-1
+		if month<1:
+			year=year-1
+			month=12
+		GetDaysInMonth(month)
+		day=day-GlobalDate.DaysInMonth
+	if year>=GlobalDate.LowerYear:
+		if month>=GlobalDate.LowerMonth:
+			if day>=GlobalDate.LowerDay:
+				randomize()
+				GlobalDate.CautionLevel6=int(rand_range(1,5))
+			else:
+				GlobalDate.CautionLevel6=0
+		else:
+			GlobalDate.CautionLevel6=0
+	else:
+		GlobalDate.CautionLevel6=0
+	Change_colour()
 
 func Change_colour():
-	randomize()
-	GlobalDate.CautionLevel6=int(rand_range(0,5))
-	if GlobalDate.CautionLevel6==0:
+	var CautionLevel=GlobalDate.CautionLevel6
+	if CautionLevel==0:
 		set_button_icon(Icon_grey)
-	elif GlobalDate.CautionLevel6==1:
+	elif CautionLevel==1:
 		set_button_icon(Icon_Green)
-	elif GlobalDate.CautionLevel6==2:
+	elif CautionLevel==2:
 		set_button_icon(Icon_Yellow)
-	elif GlobalDate.CautionLevel6==3:
+	elif CautionLevel==3:
 		set_button_icon(Icon_Orange)
-	elif GlobalDate.CautionLevel6==4:
+	elif CautionLevel==4:
 		set_button_icon(Icon_Red)
-	elif GlobalDate.CautionLevel6==5:
+	elif CautionLevel==5:
 		set_button_icon(Icon_Really_Red)
 
 func _ready():
+	var day=GlobalDate.StartDay-2
+	var month=GlobalDate.StartMonth
+	var year=GlobalDate.StartYear
+	if day<1:
+		month=month-1
+		if month<1:
+			year=year-1
+			month=12
+		GetDaysInMonth(month)
+		day=day+GlobalDate.DaysInMonth
+	if year<GlobalDate.LowerYear:
+		GlobalDate.CautionLevel6=0
+	else:
+		if month<GlobalDate.LowerMonth:
+			GlobalDate.CautionLevel6=0
+		else:
+			if day<GlobalDate.LowerDay:
+				GlobalDate.CautionLevel6=0
+			else:
+				randomize()
+				GlobalDate.CautionLevel6=int(rand_range(1,5))
 	Change_colour()
 
 func _on_LeftArrow_pressed():
-	Change_colour()
-	pass # Replace with function body.
-
+	updateColourLeft()
+	
 func _on_RightArrow_pressed():
-	Change_colour()
-	pass # Replace with function body.
+	UpdateColourRight()
+	
+func GetDaysInMonth(month):
+	if month==2:#checks if month has 28 days and if it does sends 28 back
+		GlobalDate.DaysInMonth=28
+
+	elif month==4:# or 6 or 11 or 9:#checks if month has 30 days and if it does sends 30 back
+		GlobalDate.DaysInMonth=30
+	elif month==6:# or 6 or 11 or 9:#checks if month has 30 days and if it does sends 30 back
+		GlobalDate.DaysInMonth=30
+	elif month==9:# or 6 or 11 or 9:#checks if month has 30 days and if it does sends 30 back
+		GlobalDate.DaysInMonth=30
+	elif month==11:# or 6 or 11 or 9:#checks if month has 30 days and if it does sends 30 back
+		GlobalDate.DaysInMonth=30
+
+	elif month==1: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
+	elif month==3: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
+	elif month==5: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
+	elif month==7: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
+	elif month==8: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
+	elif month==10: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
+	elif month==11: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 30 days and if it does sends 30 back
+		GlobalDate.DaysInMonth=30
+	elif month==12: #or 3 or 5 or 7 or 8 or 10 or 12:#checks if month has 31 days and if it does sends 31 back
+		GlobalDate.DaysInMonth=31
