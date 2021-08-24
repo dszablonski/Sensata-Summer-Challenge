2f73126197207554a021b66f546df1a361578174
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-24 10:39:47 +0100

    Feat: highlight currently selected day

--- src/ui/timeline/timeline.gd
+++ src/ui/timeline/timeline.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 onready var left_arrow: Button = $TopRow/LeftArrow
+onready var date_time: DateTimeLabel = $TopRow/DateTime
 onready var right_arrow: Button = $TopRow/RightArrow
 onready var days_hbox: HBoxContainer = $Days/DaysMargin/DaysHBox
 
@@ -9,3 +10,4 @@ func _ready() -> void:
 	for day in days_hbox.get_children():
 		left_arrow.connect("pressed", day, "_on_LeftArrow_pressed")
 		right_arrow.connect("pressed", day, "_on_RightArrow_pressed")
+		day.connect("pressed", date_time, "_on_Button_pressed", [day])
