657b166feafb11398fa92ade140572004849e8af
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-08-27 15:06:29 +0100

    Docs: add more DatabaseFetch comments
    Co-Authored-By: 1Tho100 <88325136+1Tho100@users.noreply.github.com>

--- src/autoload/database_fetch.gd
+++ src/autoload/database_fetch.gd
@@ -11,6 +11,7 @@ func _ready():
 	# The database must be copied from res:// to user:// as res:// is read-only.
 	# user:// is accessible at AppData/Roaming/Godot/app_userdata/Truck Viewer/
 	directory.copy(res_db_path, db_path)
+	# Opens the path to the database to allow access
 	db = SQLite.new()
 	db.path = db_path
 	db.open_db(db)
@@ -18,12 +19,17 @@ func _ready():
 
 func read_db(year: int, month: int, day: int, hour: int) -> Dictionary:
 	var hourtoget
+	# Calculate the day of year based on the month
 	var monthtotals = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+	# Calculate the day number
 	var days = ((year - 2021) * 365) + (monthtotals[month - 1])
 	if year == 2021:
+		# Adjust to the start of the database, 1/6/2021
 		days = days - 151
 	days = days + day
+	# Calculate the hour number
 	hourtoget = ((days - 1) * 24) + hour + 1
+	# Fetch data for that hour
 	db.query("select * from " + tablename + " where ID=" + str(hourtoget))
 	return db.query_result[0]
 
